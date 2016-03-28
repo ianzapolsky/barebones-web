8962cb9b2e3586ef0352271206b8bff7bdc69eba
izapolsky <izapolsky@appnexus.com>
2016-03-28 16:22:17 -0400

    remove canvas

--- js/src/views/app.js
+++ js/src/views/app.js
@@ -6,18 +6,12 @@ define([
 
   var AppView = Backbone.View.extend({
 
-    el: 'body', 
+    el: 'body',
 
     initialize: function() {
-      var canvas = document.getElementById('c');
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
-
-      var context = canvas.getContext('2d');
-      context.fillStyle = '#000';
-      context.fillRect(0, 0, canvas.width, canvas.height);
+      console.log('hello world');
     }
-  
+
   });
 
   return AppView;
